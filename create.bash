211441eeaeeafd95bd849b81166e46ddb630760a
Your Name <cyberforge1@gmail.com>
2026-02-04 12:35:31 +1100

    edits to config

--- create.bash
+++ create.bash
@@ -1,13 +1,15 @@
 # create.bash
 
-### OPENWEB UI (No Auth + Ollama)
+### OPENWEB UI (Auth + Default User Role)
 
 docker run -d \
   --name open-webui \
+  --network <docker-network> \
   -p 3000:8080 \
-  --add-host=host.docker.internal:host-gateway \
-  -v ollama:/root/.ollama \
   -v open-webui:/app/backend/data \
+  -e OLLAMA_BASE_URL=http://<ollama-host>:11434 \
   -e WEBUI_AUTH=true \
+  -e ENABLE_SIGNUP=true \
+  -e DEFAULT_USER_ROLE=user \
   --restart always \
-  ghcr.io/open-webui/open-webui:ollama
+  ghcr.io/open-webui/open-webui:main
